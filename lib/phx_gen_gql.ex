e2a8878fb004fff0d7ee7a8686d9f8c45e47e9fa
Aayush Thapa <aayushthapa7@gmail.com>
2026-01-02 14:37:30 +0545

    Initial commit

---
+++ lib/phx_gen_gql.ex
@@ -0,0 +1,18 @@
+defmodule PhxGenGql do
+  @moduledoc """
+  Documentation for `PhxGenGql`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxGenGql.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_gen_gql_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxGenGqlTest do
+  use ExUnit.Case
+  doctest PhxGenGql
+
+  test "greets the world" do
+    assert PhxGenGql.hello() == :world
+  end
+end
