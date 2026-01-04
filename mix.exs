77139802489a1065873a35279e95dc7224053941
Aayush Thapa <aayushthapa7@gmail.com>
2026-01-04 17:44:34 +0545

    chore: prepare for hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule GqlGen.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "GqlGen",
+      source_url: "https://github.com/aathapa/gql_gen"
     ]
   end
 
@@ -21,8 +25,21 @@ defmodule GqlGen.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description do
+    "A generic GraphQL generator for Phoenix applications using Absinthe."
+  end
+
+  defp package do
+    [
+      name: "gql_gen",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/aathapa/gql_gen"}
     ]
   end
 end
