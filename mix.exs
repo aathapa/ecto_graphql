e37b1dfb03d9cf442b98f61bdea9473c3cf98e9c
Aayush Thapa <aayushthapa7@gmail.com>
2026-01-05 12:12:52 +0545

    feat: lower elixir version requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EctoGraphql.MixProject do
     [
       app: :ecto_graphql,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
